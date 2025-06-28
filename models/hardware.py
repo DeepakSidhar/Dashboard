66d83c576d7ca276d5cd205d9b3b95e42debba4d
DeepakSidhar <100461868+DeepakSidhar@users.noreply.github.com>
2025-06-28 09:52:26 +0800

    Remove unique from hardware.

--- models/hardware.py
+++ models/hardware.py
@@ -5,8 +5,8 @@ class Hardware(db.Model): # user class is a subclass of  db.Model
     __tablename__ = 'hardware'  # optional this is to id the table
     id = db.Column(db.Integer, primary_key=True)# needs to e a forgnn key TODO
     name = db.Column(db.String(80), unique=True, nullable=False)
-    type = db.Column(db.String(120), unique=True, nullable=False)
-    manufacturer = db.Column(db.String(80), unique=True, nullable=False)
+    type = db.Column(db.String(120), nullable=False)
+    manufacturer = db.Column(db.String(80),  nullable=False)
     model  = db.Column(db.String(80),  nullable=False)
     location = db.Column(db.String(80),  nullable=False)
     status = db.Column(db.String(50), nullable=False)
