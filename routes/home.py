01d80f32cf364e46846d6d051fa358808346f845
DeepakSidhar <100461868+DeepakSidhar@users.noreply.github.com>
2025-07-22 10:23:48 +0800

    Add vulnerability summary.

--- routes/home.py
+++ routes/home.py
@@ -1,10 +1,28 @@
 from flask import Blueprint, jsonify, render_template, request, session, redirect, g
+from sqlalchemy import func
+
 from auth import login_required, role_required, login_session_required
-from models import User
+from models import User, db, Vulnerability, Software
 
 home_bp = Blueprint('home', __name__)
 
 @home_bp.route('/', methods=['GET' ])
 @login_session_required
 def home():
-    return render_template('home.html', user=g.user)
+    summary = (
+        db.session.query(
+            Vulnerability.severity,
+            func.count(Vulnerability.cve_id)
+        )
+        .join(
+            Software,
+            (Software.name == Vulnerability.product) &
+            (Software.vendor == Vulnerability.vendor) &
+            (Software.version == Vulnerability.version)
+        )
+        .group_by(Vulnerability.severity)
+        .all()
+    )
+    print(summary)
+    print(dict(summary))
+    return render_template('home.html', summary=dict(summary)) #Change the data structure
